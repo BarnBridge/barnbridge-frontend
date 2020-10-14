7d555c54e430425f5743e975b36f34dcda166520
Slava Matvienco <felix.wfm@gmail.com>
2020-10-14 22:49:36 +0300

    Added No Mobile Support Screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
 
 import Web3Provider from 'web3/provider';
 import Web3ContractsProvider from 'web3/contracts';
@@ -8,6 +9,17 @@ import TheGraphProvider from 'context/TheGraph';
 import LayoutView from 'views/layout';
 
 const App: React.FunctionComponent = () => {
+  if (isMobile) {
+    return (
+      <div className="mobile-not-available-wrap">
+        <p>BarnBridge</p>
+        <p>Yield Farming</p>
+        <p>is currently not available</p>
+        <p>on mobile and tablet</p>
+      </div>
+    );
+  }
+
   return (
     <Web3Provider>
       <Web3ContractsProvider>
