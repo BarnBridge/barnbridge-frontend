0221046506534022dd766e3bb6eea4c7b8079743
Slava Matvienco <slava.wfm@gmail.com>
2021-02-18 12:04:51 +0200

    Fix Pool Card

--- src/modules/yield-farming/components/pool-card/index.tsx
+++ src/modules/yield-farming/components/pool-card/index.tsx
@@ -336,36 +336,35 @@ const PoolCard: React.FunctionComponent<PoolCardProps> = props => {
                     balance and your effective pool balance will differ in most
                     cases.
                   </span>}>My Pool Balance</Hint>
-            </Label>
-            <Paragraph type="p1" semiBold color="primary">
-              {formatUSDValue(state.myBalance)}
-            </Paragraph>
+          </Label>
+          <Paragraph type="p1" semiBold color="primary">
+            {formatUSDValue(state.myBalance)}
+          </Paragraph>
 
-            {!state.isEnded && (
-              <>
-                <Paragraph type="p2" color="secondary">
-                  {formatUSDValue(state.myEffectiveBalance)} effective balance
-                </Paragraph>
-                <PoolStakeShareBar shares={state.myShares} />
-              </>
-            )}
-          </Grid>
-        )}
-        {state.isEnded && (
-          <div className={s.box}>
-            <Grid flow="row" align="start">
-              <Paragraph type="p2" semiBold color="secondary">
-                The $BOND staking pool ended after 12 epochs on Feb 08, 00:00 UTC. Deposits are now disabled, but
-                you
-                can
-                still withdraw your tokens and collect any unclaimed rewards. To continue to stake $BOND
+          {!state.isEnded && (
+            <>
+              <Paragraph type="p2" color="secondary">
+                {formatUSDValue(state.myEffectiveBalance)} effective balance
               </Paragraph>
-              <Button type="link" onClick={handleDaoStaking}>Go to governance staking</Button>
-            </Grid>
-          </div>
-        )}
-      </div>
-    </div>
+              <PoolStakeShareBar shares={state.myShares} />
+            </>
+          )}
+        </Grid>
+      )}
+      {state.isEnded && (
+        <div className={s.box}>
+          <Grid flow="row" align="start">
+            <Paragraph type="p2" semiBold color="secondary">
+              The $BOND staking pool ended after 12 epochs on Feb 08, 00:00 UTC. Deposits are now disabled, but
+              you
+              can
+              still withdraw your tokens and collect any unclaimed rewards. To continue to stake $BOND
+            </Paragraph>
+            <Button type="link" onClick={handleDaoStaking}>Go to governance staking</Button>
+          </Grid>
+        </div>
+      )}
+    </Card>
   );
 };
 
