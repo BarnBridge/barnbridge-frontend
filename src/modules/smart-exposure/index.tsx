903b1833ca59927c3a4c855880afc27c47eae361
Slava Matvienco <slava.matvienco@gmail.com>
2021-05-06 16:10:14 +0300

    Fix last PR (#231)
    * Update dependencies
    
    * Contract Polish
    
    * Refactor contracts
    
    * Polish YF module
    
    * Polish YF
    
    * Polish YF
    
    * Polish YF
    
    * YF Polish
    
    * Fix
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fix
    
    * Fix SY Pools
    
    * Fix SVG icons sprite
    
    * Fix

--- src/modules/smart-exposure/index.tsx
+++ src/modules/smart-exposure/index.tsx
@@ -5,7 +5,7 @@ import AntdSpin from 'antd/lib/spin';
 import Icon from 'components/custom/icon';
 import { NavTabs } from 'components/custom/tabs';
 import { Text } from 'components/custom/typography';
-import { ReactComponent as RocketSvg } from 'resources/svg/rocket.svg';
+import RocketSvg from 'resources/img/rocket.png';
 import { useWallet } from 'wallets/wallet';
 
 import { isDevelopment } from 'utils';
@@ -74,7 +74,7 @@ const SmartExposureView: React.FC = () => {
   if (!isDevelopment) {
     return (
       <div className="flex flow-row row-gap-48 align-center justify-center full-height">
-        <RocketSvg style={{ maxWidth: '310px', maxHeight: '377px' }} />
+        <img src={RocketSvg} alt="Rocket" style={{ maxWidth: '310px', maxHeight: '377px' }} />
         <div className="grid flow-row row-gap-8">
           <Text type="h1" weight="bold" color="primary">
             Stay tuned!
