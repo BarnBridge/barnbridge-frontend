d0a0656e4fc72b4f0e01299de7bf70c98872914c
Slava Matvienco <slava.wfm@gmail.com>
2021-05-10 12:33:42 +0300

    Fixes

--- src/modules/smart-yield/views/pools-view/index.tsx
+++ src/modules/smart-yield/views/pools-view/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AntdSpin from 'antd/lib/spin';
 import cn from 'classnames';
 import { ZERO_BIG_NUMBER, formatUSD } from 'web3/utils';
@@ -6,28 +6,25 @@ import { ZERO_BIG_NUMBER, formatUSD } from 'web3/utils';
 import Icon, { IconNames } from 'components/custom/icon';
 import { Text } from 'components/custom/typography';
 import { useKnownTokens } from 'components/providers/known-tokens-provider';
-import { mergeState } from 'hooks/useMergeState';
 import { Markets, SYMarketMeta } from 'modules/smart-yield/api';
 import { useRewardPools } from 'modules/smart-yield/providers/reward-pools-provider';
 import { PoolsCard } from 'modules/smart-yield/views/pools-view/pool-card';
 
-type State = {
-  activeMarket?: SYMarketMeta;
-};
-
-const InitialState: State = {
-  activeMarket: Array.from(Markets.values())[0],
-};
-
 const PoolsView: React.FC = () => {
   const knownTokensCtx = useKnownTokens();
-  const [state, setState] = useState<State>(InitialState);
   const rewardPoolsCtx = useRewardPools();
   const { loading, rewardPools } = rewardPoolsCtx;
 
+  const [activeMarket, setActiveMarket] = React.useState<SYMarketMeta | undefined>();
+
+  React.useEffect(() => {
+    const market = Array.from(Markets.values()).find(market => market.id === rewardPoolsCtx.rewardPools[0]?.protocolId);
+    setActiveMarket(market);
+  }, [rewardPoolsCtx.rewardPools]);
+
   const activeMarketTotalValueLocked = React.useMemo(() => {
     return rewardPools
-      .filter(pool => pool.protocolId === state.activeMarket?.id)
+      .filter(pool => pool.protocolId === activeMarket?.id)
       .reduce((sum, c) => {
         if (!c.pool.poolSize) {
           return sum;
@@ -44,36 +41,34 @@ const PoolsView: React.FC = () => {
 
         return sum.plus(usdValue);
       }, ZERO_BIG_NUMBER);
-  }, [rewardPoolsCtx, state.activeMarket]);
+  }, [rewardPoolsCtx, activeMarket]);
 
   return (
     <>
       <div className="tab-cards mb-64">
-        {Array.from(Markets.values()).map(market => (
-          <button
-            key={market.name}
-            type="button"
-            className={cn('tab-card', state.activeMarket === market && 'active')}
-            disabled={!market.active}
-            style={{ color: !market.active ? 'red' : '' }}
-            onClick={() => {
-              setState(
-                mergeState<State>({
-                  activeMarket: market,
-                }),
-              );
-            }}>
-            <Icon name={market.icon as IconNames} width={40} height={40} className="mr-16" color="inherit" />
-            <div>
-              <Text type="p1" weight="semibold" color="primary">
-                {market.name}
-              </Text>
-              <Text type="small" weight="semibold" color="secondary">
-                {market.active ? 'Markets' : 'Coming soon'}
-              </Text>
-            </div>
-          </button>
-        ))}
+        {Array.from(Markets.values()).map(market => {
+          const isActive = rewardPoolsCtx.rewardPools.some(pool => pool.protocolId === market.id);
+
+          return (
+            <button
+              key={market.name}
+              type="button"
+              className={cn('tab-card', activeMarket === market && 'active')}
+              disabled={!isActive}
+              style={{ color: !isActive ? 'red' : '' }}
+              onClick={() => setActiveMarket(market)}>
+              <Icon name={market.icon as IconNames} width={40} height={40} className="mr-16" color="inherit" />
+              <div>
+                <Text type="p1" weight="semibold" color="primary">
+                  {market.name}
+                </Text>
+                <Text type="small" weight="semibold" color="secondary">
+                  {isActive ? 'Markets' : 'Coming soon'}
+                </Text>
+              </div>
+            </button>
+          );
+        })}
       </div>
       <div className="flex mb-32">
         <div>
@@ -88,7 +83,7 @@ const PoolsView: React.FC = () => {
       <AntdSpin spinning={loading}>
         <div className="flex row-gap-32 col-gap-32">
           {rewardPools
-            .filter(pool => pool.protocolId === state.activeMarket?.id)
+            .filter(pool => pool.protocolId === activeMarket?.id)
             .map(rewardPool => (
               <PoolsCard key={rewardPool.poolAddress} rewardPool={rewardPool} />
             ))}
