180baf4996fc930411a140ec2509abc97ef10452
Alexandr Ricov <rykovalexandr@gmail.com>
2021-05-17 11:25:59 +0300

    fix merge issues

--- src/config.ts
+++ src/config.ts
@@ -60,22 +60,7 @@ const config = {
       dai: toLowerCase(process.env.REACT_APP_TESTNET_AAVE_DAI_ADDR),
       usdt: toLowerCase(process.env.REACT_APP_TESTNET_AAVE_USDT_ADDR),
     },
-    testnet: {
-      comp: {
-        fauceteer: String(process.env.REACT_APP_CONTRACT_TESTNET_COMP_FAUCETEER_ADDR).toLowerCase(),
-        usdc: String(process.env.REACT_APP_CONTRACT_TESTNET_COMP_USDC_ADDR).toLowerCase(),
-        dai: String(process.env.REACT_APP_CONTRACT_TESTNET_COMP_DAI_ADDR).toLowerCase(),
-      },
-      aave: {
-        fauceteer: String(process.env.REACT_APP_CONTRACT_TESTNET_AAVE_FAUCETEER_ADDR).toLowerCase(),
-        usdc: String(process.env.REACT_APP_CONTRACT_TESTNET_AAVE_USDC_ADDR).toLowerCase(),
-        dai: String(process.env.REACT_APP_CONTRACT_TESTNET_AAVE_DAI_ADDR).toLowerCase(),
-        usdt: String(process.env.REACT_APP_CONTRACT_TESTNET_AAVE_USDT_ADDR).toLowerCase(),
-      },
-      bond: String(process.env.REACT_APP_CONTRACT_TESTNET_BOND_ADDR).toLowerCase(),
-    },
   },
-
   web3: {
     chainId: Number(process.env.REACT_APP_WEB3_CHAIN_ID),
     poolingInterval: Number(process.env.REACT_APP_WEB3_POLLING_INTERVAL),
