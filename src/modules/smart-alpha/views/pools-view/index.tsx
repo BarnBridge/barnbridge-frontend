e20d8791eed1b32bd4772f12aa56e82128e2c445
Slava Matvienco <slava.matvienco@gmail.com>
2021-09-09 16:43:29 +0300

    Feature/sa (#404)
    * Fix SA Simulate view
    
    * Fix SA Simulate view
    
    * fix layout issues
    
    * remove unused imports
    
    * Fix SA Simulate screen
    
    * accept Simulate btn for not logged
    
    * *
    
    * update texts
    
    * restrict Launch page only
    
    * *
    
    * Add SA Loupe contract
    
    * Add SA Loupe contract
    
    * connect new api to previous epochs
    
    * fix queued positions close
    
    * SA Fixes
    
    * switch to alpha-v2
    
    * add pool epoch spinner
    
    * Fix SA leverages values
    
    * fix loaders
    
    * *
    
    * polish final
    
    * switch to api-v2
    
    * fix pool card progress time
    
    * Fix web3 version
    
    * Fix SA Pool view
    
    * Fix package-lock.json
    
    * Fix package-lock.json
    
    * Fix minor issues
    
    * Fix leverages
    
    * SA fixes
    
    * SA fixes
    
    * SA fixes
    
    * SA fixes
    
    Co-authored-by: Alexandr Ricov <rykovalexandr@gmail.com>

--- src/modules/smart-alpha/views/pools-view/index.tsx
+++ src/modules/smart-alpha/views/pools-view/index.tsx
@@ -34,7 +34,7 @@ const PoolsView = () => {
     }, BigNumber.ZERO);
   }, [data]);
 
-  const exitQueueTVLInUsd = useMemo(() => {
+  const exitedTVLInUsd = useMemo(() => {
     return data?.reduce((sum, item) => {
       return sum.plus(item.tvl.juniorExitedTVL).plus(item.tvl.seniorExitedTVL);
     }, BigNumber.ZERO);
@@ -63,10 +63,10 @@ const PoolsView = () => {
 
         <div className="card p-24" style={{ minWidth: '200px' }}>
           <Text type="small" weight="semibold" color="secondary" className="mb-4">
-            Exit Queue TVL
+            Exited TVL
           </Text>
           <Text type="h3" weight="bold" color="primary">
-            {formatUSD(exitQueueTVLInUsd) ?? '-'}
+            {formatUSD(exitedTVLInUsd) ?? '-'}
           </Text>
         </div>
       </div>
