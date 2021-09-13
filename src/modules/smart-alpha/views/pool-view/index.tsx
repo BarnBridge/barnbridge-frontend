cabbf7f092df15960d346befee65a018b507a824
Slava Matvienco <slava.matvienco@gmail.com>
2021-09-13 17:52:01 +0300

    Fix SA Timer; Fix SA decimals (#407)

--- src/modules/smart-alpha/views/pool-view/index.tsx
+++ src/modules/smart-alpha/views/pool-view/index.tsx
@@ -325,10 +325,6 @@ const PoolView = () => {
                     {formatToken(
                       smartAlphaContract?.epochSeniorLiquidity?.unscaleBy(pool.poolToken.decimals) ??
                         BigNumber.from(pool.state.seniorLiquidity),
-                      {
-                        decimals: pool.poolToken.decimals,
-                        // compact: true,
-                      },
                     ) ?? '-'}
                   </Text>
                   <TokenIcon name={poolToken?.icon ?? 'unknown'} size={16} />
@@ -372,10 +368,7 @@ const PoolView = () => {
                       </Text>
                     }
                     className="mr-8">
-                    {formatToken(smartAlphaContract?.epochJuniorLiquidity?.unscaleBy(pool.poolToken.decimals), {
-                      decimals: pool.poolToken.decimals,
-                      // compact: true,
-                    }) ?? '-'}
+                    {formatToken(smartAlphaContract?.epochJuniorLiquidity?.unscaleBy(pool.poolToken.decimals)) ?? '-'}
                   </Text>
                   <TokenIcon name={poolToken?.icon ?? 'unknown'} size={16} />
                 </dd>
@@ -540,10 +533,7 @@ const PoolView = () => {
                       </Text>
                     }
                     className="mr-8">
-                    {formatToken(nextEpochEstimates[1]?.unscaleBy(pool.poolToken.decimals), {
-                      decimals: pool.poolToken.decimals,
-                      // compact: true,
-                    }) ?? '-'}
+                    {formatToken(nextEpochEstimates[1]?.unscaleBy(pool.poolToken.decimals)) ?? '-'}
                   </Text>
                   <TokenIcon name={poolToken?.icon ?? 'unknown'} size={16} />
                 </dd>
@@ -581,10 +571,7 @@ const PoolView = () => {
                       </Text>
                     }
                     className="mr-8">
-                    {formatToken(nextEpochEstimates[0]?.unscaleBy(pool.poolToken.decimals), {
-                      decimals: pool.poolToken.decimals,
-                      // compact: true,
-                    }) ?? '-'}
+                    {formatToken(nextEpochEstimates[0]?.unscaleBy(pool.poolToken.decimals)) ?? '-'}
                   </Text>
                   <TokenIcon name={poolToken?.icon ?? 'unknown'} size={16} />
                 </dd>
