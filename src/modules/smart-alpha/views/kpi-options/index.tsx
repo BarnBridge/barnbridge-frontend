b0a907f925be295a97fcb45b8ab1099b10993f42
Slava Matvienco <slava.matvienco@gmail.com>
2021-10-08 17:22:47 +0300

    Feature/sa kpi options (#441)
    * copy se pools
    
    * add multiple outline color for token icon
    
    * Adjust KpiOptions view
    
    * Add KPI Option View
    
    * Finish KpiOption View
    
    * Small Fixes
    
    * connect token icons
    
    * display KPIOptions only on ethereum networks
    
    * Support tokens price for diff networks
    
    * Token price fix
    
    * Small Fixes
    
    * Fix
    
    * Fixes
    
    * Fix Kpi View
    
    Co-authored-by: Alexandr Ricov <rykovalexandr@gmail.com>

--- src/modules/smart-alpha/views/kpi-options/index.tsx
+++ src/modules/smart-alpha/views/kpi-options/index.tsx
@@ -4,14 +4,18 @@ import AntdSpin from 'antd/lib/spin';
 import { useFetchKpiOptions } from 'modules/smart-alpha/api';
 import { KpiOptionCard } from 'modules/smart-alpha/views/kpi-options/kpi-option-card';
 
+import s from './s.module.scss';
+
 const KpiOptionsView: FC = () => {
   const { loading, data: kpiOptions } = useFetchKpiOptions();
 
   return (
     <AntdSpin spinning={loading}>
-      {kpiOptions?.map(kpiOption => (
-        <KpiOptionCard key={kpiOption.poolAddress} kpiOption={kpiOption} />
-      ))}
+      <div className={s.cards}>
+        {kpiOptions?.map(kpiOption => (
+          <KpiOptionCard key={kpiOption.poolAddress} kpiOption={kpiOption} />
+        ))}
+      </div>
     </AntdSpin>
   );
 };
