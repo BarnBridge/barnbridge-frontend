e66beec7a9257fae215375c92e3b2611f68c12e7
syrohei <syrohei@gmail.com>
2021-12-19 15:38:45 +0800

    feat: build

---
+++ build/static/js/58.0f5d1bc5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbarnbridge-frontend"]=this["webpackJsonpbarnbridge-frontend"]||[]).push([[58],{2003:function(n,b){}}]);
+//# sourceMappingURL=58.0f5d1bc5.chunk.js.map
---
+++ build/static/js/59.25b198bc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbarnbridge-frontend"]=this["webpackJsonpbarnbridge-frontend"]||[]).push([[59],{1922:function(n,b){}}]);
+//# sourceMappingURL=59.25b198bc.chunk.js.map
